d6fc07d7f2ec0aa782fee70644e9426066d0a82a
VarunKvK <varunkrishnankv2002@gmail.com>
2026-07-25 16:25:16 +0530

    feat(analytics): Integrate Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -36,7 +37,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.className} scroll-smooth`}>
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MK1QVPVPSJ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-MK1QVPVPSJ');`}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
